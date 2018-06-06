c99a876928d768f2114779b530ffc5d88db26047
Jonathan Drake <jdrake@narrativescience.com>
2018-06-06 13:54:58 -0700

    ShowPageAction

--- background.js
+++ background.js
@@ -2,6 +2,24 @@
  * Background Page
  */
 
+chrome.runtime.onInstalled.addListener(() => {
+  if (chrome.declarativeContent == null) {
+    return
+  }
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'calendar.google.com' },
+          })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ])
+  })
+})
+
 chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
   if (details.url === 'https://calendar.google.com/calendar/r/eventedit' &&
       details.transitionQualifiers != null &&
