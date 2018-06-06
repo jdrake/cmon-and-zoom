4d8d43b9923992ff181ee0b119cfcaeebac7fcd8
Jonathan Drake <jdrake@narrativescience.com>
2018-06-06 11:59:26 -0700

    boolean

--- background.js
+++ background.js
@@ -5,7 +5,7 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
   if (details.url === 'https://calendar.google.com/calendar/r/eventedit' &&
       details.transitionQualifiers != null &&
-      !!details.transitionQualifiers.length
+      !details.transitionQualifiers.length
   ) {
     chrome.tabs.sendMessage(details.tabId, {
       message: 'NEW_EVENT',
